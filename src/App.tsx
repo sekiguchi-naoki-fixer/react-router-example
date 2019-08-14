2c624d897fbeabe23538ca47f333798cae6714f4
Naoki Sekiguchi <sekiguchi.naoki@fixer.co.jp>
2019-08-14 12:48:06 +0900

    Implement basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Page3 from './Page3';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul className="nav">
+        <li><a href="/Page1">Page1</a></li>
+        <li><a href="/Page2">Page2</a></li>
+        <li><a href="/Page3">Page3</a></li>
+      </ul>
+      <Switch>
+        <Route path='/Page1' component={Page1} />
+        <Route path='/Page2' component={Page2} />
+        <Route path='/Page3' component={Page3} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Page1.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const Page1: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Page1</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Page1;
